e06c7dd7d297340cda81cb2475bda1388335e811
deogsin <46525991+deogsin@users.noreply.github.com>
2019-01-09 21:38:29 +0900

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,123 @@
+전남과학대 
+
+wbs 웹서버 가상화 
+
+
+was 환경
+iis 서버 설치 인터넷 정보관리자
+
+
+vmware sphere client 
+-사용
+
+자주사용 명령어 
+
+crontab
+ps -eflgrep it 
+ifconfig 
+
+-eflgrep blackbox
+ps -efigreppostgres
+
+service postgresql -lo start 
+
+ps -eflgrep oracle
+
+su -oracle
+sqlplus webmaster
+desc wbb_app_server
+
+cd save
+cd wlogs_t
+cd../backup/save
+
+ls
+
+ps -eflgrep tt
+df -h
+
+data-data
+
+http://weeds.server.weeds.co.kr/3000
+
+id/password 제품배포 -문서- web 디스커버  
+
+weeds 폴더 -> 저장 --> 날
+짜별폴더지정
+inet addr /root/port:22/
+
+
+디스커버 인스톨 메
+뉴얼
+dbms - r-db postregrsql
+
+discover -> root@59.170.~~
+
+echor@ mirror switch 
+
+echor
+tcpdump -i echo host ip and port:
+
+
+post gresql 설치 
+
+cd install 
+
+tar -jxvf pg i<centos_tar_rpm
+
+rpm -Uvh. pgdg-centoslo-lo-1
+
+Postresql DB 
+
+elastic serach
+
+usr/local/java
+
+엘라스틱 자바설정 ,빔에디터 설정 ,블랙박스 유
+저추가
+discover, wastrace 
+
+
+ps eflgrep ENGINE 
+
+tcpdump -1 em2 host ip
+
+waas  web as a sevice 
+
+kibana 
+
+log stash
+metric beat 설치 
+기존서버-> wbs서버 수
+집
+
+sql select * from 
+
+cd logs
+ls-ltr
+IIS -Trace 메
+뉴얼
+OS별 서버 
+
+시스템변경, WBS서버 WAS가능하게 설치    
+
+work.weeds.co.kr
+
+
+iisreset -> iis 서버 재
+기동
+윈도우 서버 이벤트 뷰
+어
+로그생성 --> 추적, 테스트되는 지 추
+적
+sbin 편집 
+
+
+cd wlogs 
+
+
+iis-> 콘솔루트  
+작업스케줄러 (로컬)
+
+IIS작업 5분 무기한 
+Sbin /pro.beat
